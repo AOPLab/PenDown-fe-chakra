abacf111ac6f147ca49d97c82a29650a4467af69
Brian L. Chen <brian.lxchen@gmail.com>
2022-05-01 15:15:06 +0800

    Fix profile width

--- src/containers/account/profile.js
+++ src/containers/account/profile.js
@@ -13,7 +13,7 @@ function PersonalProfile() {
 
   return (
     <Box pt="20">
-      <Container maxW="6xl">
+      <Container maxW="5xl">
         <Flex
           w="full"
           justifyContent="center"
@@ -27,7 +27,7 @@ function PersonalProfile() {
             mx="auto"
           >
             <NoteCard imageUrl="https://p.calameoassets.com/180515111509-087734d3ab9181b3dbabd2c3eab490b6/p1.jpg" />
-            <NoteCard imageUrl="https://www.myperfectresume.com/wp-content/uploads/2021/07/accentuate_intern.svg" />
+            <NoteCard imageUrl="https://img.freepik.com/free-psd/landscape-cover-mock-up-template-a4-size_165833-912.jpg" />
             <NoteCard imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/I-20-sample.pdf/page1-463px-I-20-sample.pdf.jpg" />
             <NoteCard imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGUp_6A6lTrhauVsYhgjXQl0hrbr2nkjveomha7GkGPksnRdWCKJwbAbToS_3ku3giebw&usqp=CAU" />
             <NoteCard imageUrl="https://imgv2-2-f.scribdassets.com/img/document/262443256/original/acb42c6feb/1613379183?v\u003d1" />
