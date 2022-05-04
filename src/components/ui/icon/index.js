59bc1bb87f8bc6edf0b809998222f683d2f4eb93
Brian L. Chen <brian.lxchen@gmail.com>
2022-05-04 21:17:27 +0800

    Update tabs

--- src/components/ui/icon/index.js
+++ src/components/ui/icon/index.js
@@ -90,6 +90,7 @@ import { ReactComponent as Search } from './search.svg';
 import { ReactComponent as Notification } from './notification.svg';
 import { ReactComponent as AddButton } from './add_button.svg';
 import { ReactComponent as NoteBean } from './Note-Bean-Black.svg';
+import { ReactComponent as NoteBeanOriginal } from './note-bean-original.svg';
 
 export default {
 //   HistoryIcon,
@@ -181,4 +182,5 @@ export default {
   Notification,
   AddButton,
   NoteBean,
+  NoteBeanOriginal,
 };
