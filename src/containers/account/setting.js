30b04db75acebc152e0ed1f4f4fe32e910ee1655
gary1030 <gary6658@ntu.im>
2022-05-04 12:14:57 +0800

    API: edit account

--- src/containers/account/setting.js
+++ src/containers/account/setting.js
@@ -1,12 +1,32 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Container, Box, Heading, Button,
+} from '@chakra-ui/react';
+import { editAccount } from '../../actions/user/user';
 
 function AccountSetting() {
   // const history = useHistory();
   // const location = useLocation();
-  // const config = useSelector((state) => state.auth);
-  // const user = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
+  const config = useSelector((state) => state.auth);
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');
+
+  const onSubmit = () => {
+    dispatch(editAccount(config.token, username, fullName, email, description));
+  };
+
+  useEffect(() => {
+    setUsername(user.username);
+    setFullName(user.fullName);
+    setEmail(user.email);
+    setDescription(user.description);
+  }, [user.description, user.email, user.fullName, user.username]);
 
   return (
     <Box borderBottom="2px solid black" pt="20">
@@ -15,13 +35,16 @@ function AccountSetting() {
         <h1>This is container about setting of account.</h1>
         <h1>This is container about setting of account.</h1>
         <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
-        <h1>This is container about setting of account.</h1>
+        <h1>{username}</h1>
+        <h1>{fullName}</h1>
+        <h1>{email}</h1>
+        <h1>{description}</h1>
         {/* <AccountSetting /> */}
+        <Button
+          onClick={onSubmit}
+        >
+          Submit
+        </Button>
 
       </Container>
     </Box>
