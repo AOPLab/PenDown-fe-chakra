d12a3cb6922096fcedbfd0bb5ee38c6342d51865
gary1030 <gary6658@ntu.im>
2022-05-04 13:21:55 +0800

    Init: get note api

--- src/containers/note/index.js
+++ src/containers/note/index.js
@@ -1,5 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   // Container,
   Box,
@@ -15,33 +16,68 @@ import DescriptionSection from '../../components/ui/notepage/DescriptionSection'
 import CourseSection from '../../components/ui/notepage/CourseSection';
 import TagsSection from '../../components/ui/notepage/TagsSection';
 
-const property = {
-  pdfUrl: 'http://www.africau.edu/images/default/sample.pdf',
-  imageUrl: 'https://p.calameoassets.com/180515111509-087734d3ab9181b3dbabd2c3eab490b6/p1.jpg',
-  imageAlt: 'Alt',
-  dateCreated: 'Mar. 12, 2022',
-  title: 'IM 3007 Midterm Note',
-  formattedPrice: '$1,900.00',
-  description: 'The description of the note. Users can explain as much as he/she wants in this section.',
-  school: 'National Taiwan University',
-  course: 'IM 3007: System Analysis and Design',
-  reviewCount: 34,
-  viewCount: '3.2k',
-  savedCount: '32',
-  notability: true,
-  noteType: 'Notability',
-  username: 'cutey',
-  fullName: 'Cindy L. Jhou',
-  template: true,
-};
+import { getNote } from '../../actions/note/note';
 
 function Note() {
   const { noteId } = useParams();
   // const history = useHistory();
   // const location = useLocation();
-  // const config = useSelector((state) => state.auth);
+  const config = useSelector((state) => state.auth);
+  const notes = useSelector((state) => state.note.byId);
   // const user = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
+
+  const [property, setProperty] = useState({
+    imageUrl: 'https://p.calameoassets.com/180515111509-087734d3ab9181b3dbabd2c3eab490b6/p1.jpg',
+    imageAlt: 'Alt',
+    dateCreated: 'Mar. 12, 2022',
+    title: 'IM 3007 Midterm Note',
+    formattedPrice: '$1,900.00',
+    description: 'The description of the note. Users can explain as much as he/she wants in this section.',
+    school: 'National Taiwan University',
+    course: 'IM 3007: System Analysis and Design',
+    reviewCount: 34,
+    viewCount: '3.2k',
+    savedCount: '32',
+    notability: true,
+    noteType: 'Notability',
+    username: 'cutey',
+    fullName: 'Cindy L. Jhou',
+    template: true,
+  });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (notes[noteId]) {
+      setProperty({
+        imageUrl: notes[noteId].preview_url,
+        imageAlt: 'No Preview Image!',
+        dateCreated: notes[noteId].created_at,
+        title: notes[noteId].title,
+        formattedPrice: notes[noteId].bean,
+        description: notes[noteId].description,
+        school: notes[noteId].school_name,
+        course: `${notes[noteId].course_no} ${notes[noteId].course_name}`,
+        viewCount: notes[noteId].view_cnt,
+        savedCount: notes[noteId].saved_cnt,
+        notability: true,
+        noteType: notes[noteId].note_type,
+        userId: notes[noteId].account_id,
+        username: notes[noteId].username,
+        fullName: notes[noteId].username,
+        template: notes[noteId].is_template,
+      });
+    }
+  }, [noteId, notes]);
+
+  useEffect(() => {
+    if (noteId !== null && Number.isInteger(Number(noteId))) {
+      if (config.token !== null && config.token !== '') {
+        dispatch(getNote(noteId, config.token));
+      } else {
+        dispatch(getNote(noteId));
+      }
+    }
+  }, [config.token, dispatch, noteId]);
 
   return (
     <>
