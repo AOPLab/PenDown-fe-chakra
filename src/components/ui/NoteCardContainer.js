b1f54cd2351b007c35e39898de1b1126dd2d94da
Chienyu-1215 <b08705034@ntu.edu.tw>
2022-05-05 11:06:00 +0800

    commit changes

---
+++ src/components/ui/NoteCardContainer.js
@@ -0,0 +1,32 @@
+import {
+  Flex, SimpleGrid, Container,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import NoteCard from './NoteCard';
+
+export default function NoteCardContainer(property) {
+  return (
+    <Container maxW="6xl">
+      <Flex
+        w="full"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacing={10}
+          px={{ base: 4, lg: 8, xl: 12 }}
+          py={20}
+          mx="auto"
+        >
+          <NoteCard imageUrl="https://p.calameoassets.com/180515111509-087734d3ab9181b3dbabd2c3eab490b6/p1.jpg" />
+          <NoteCard imageUrl="https://www.myperfectresume.com/wp-content/uploads/2021/07/accentuate_intern.svg" />
+          <NoteCard imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/I-20-sample.pdf/page1-463px-I-20-sample.pdf.jpg" />
+          <NoteCard imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGUp_6A6lTrhauVsYhgjXQl0hrbr2nkjveomha7GkGPksnRdWCKJwbAbToS_3ku3giebw&usqp=CAU" />
+          <NoteCard imageUrl="https://imgv2-2-f.scribdassets.com/img/document/262443256/original/acb42c6feb/1613379183?v\u003d1" />
+          <NoteCard imageUrl="https://img.yumpu.com/21400799/1/500x640/download-pdf-file-of-sample-newsletter-articles-get-involved.jpg" />
+        </SimpleGrid>
+      </Flex>
+    </Container>
+  );
+}
--- src/containers/account/profile.js
+++ src/containers/account/profile.js
@@ -1,41 +1,59 @@
 import {
-  Box, Container, Flex, SimpleGrid,
+  Box, 
+  HStack,
+  VStack,
+  Text,
+  Avatar,
+  Container,
 } from '@chakra-ui/react';
 import React from 'react';
-import NoteCard from '../../components/ui/NoteCard';
+import { useSelector } from 'react-redux';
+import NoteCardContainer from '../../components/ui/NoteCardContainer';
 
 function PersonalProfile() {
   // const history = useHistory();
   // const location = useLocation();
   // const config = useSelector((state) => state.auth);
-  // const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
   // const dispatch = useDispatch();
 
   return (
-    <Box pt="20">
-      <Container maxW="6xl">
-        <Flex
-          w="full"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <SimpleGrid
-            columns={{ base: 1, md: 2, lg: 3 }}
-            spacing={10}
-            px={{ base: 4, lg: 8, xl: 12 }}
+    <>
+      <Container
+        width="10xl"
+        pt="10px"
+      >
+        <HStack>
+          <Container
+            // mx="auto"
+            // px={{ base: 4, lg: 8, xl: 12 }}
             py={20}
-            mx="auto"
+            // textAlign="center"
+            alignContent="center"
           >
-            <NoteCard imageUrl="https://p.calameoassets.com/180515111509-087734d3ab9181b3dbabd2c3eab490b6/p1.jpg" />
-            <NoteCard imageUrl="https://www.myperfectresume.com/wp-content/uploads/2021/07/accentuate_intern.svg" />
-            <NoteCard imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/I-20-sample.pdf/page1-463px-I-20-sample.pdf.jpg" />
-            <NoteCard imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGUp_6A6lTrhauVsYhgjXQl0hrbr2nkjveomha7GkGPksnRdWCKJwbAbToS_3ku3giebw&usqp=CAU" />
-            <NoteCard imageUrl="https://imgv2-2-f.scribdassets.com/img/document/262443256/original/acb42c6feb/1613379183?v\u003d1" />
-            <NoteCard imageUrl="https://img.yumpu.com/21400799/1/500x640/download-pdf-file-of-sample-newsletter-articles-get-involved.jpg" />
-          </SimpleGrid>
-        </Flex>
+            <Avatar
+              border="2px solid black"
+              height="auto"
+              width="8vw"
+              src={`https://source.boringavatars.com/beam/40/${user.username}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`}
+            />
+            <Text py="5px" fontSize={{ base: 'm', sm: 's', md: 'm' }} fontWeight={900} lineHeight="150%">
+              {user.username}
+            </Text>
+          </Container>
+          <VStack>
+            <HStack>
+              <Box borderRadius="pendown" border="2px black">
+                kd
+              </Box>
+            </HStack>
+          </VStack>
+        </HStack>
       </Container>
-    </Box>
+      <Box pt="20">
+        <NoteCardContainer />
+      </Box>
+    </>
   );
 }
 
