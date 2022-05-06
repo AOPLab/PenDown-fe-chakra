aad35060ca15c1b2d3e4150688846fc455048b1f
gary1030 <gary6658@ntu.im>
2022-05-07 00:59:26 +0800

    sidebar adjustment

--- src/containers/search/index.js
+++ src/containers/search/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
-import {
-  Switch, Route, // useHistory, useLocation,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // import { makeStyles } from '@material-ui/core';
+import {
+  Box, Flex,
+} from '@chakra-ui/react';
 
+import SimpleSideBar from '../../components/ui/sidebar/SimpleSideBar';
 import People from './people';
 import Tags from './tags';
 import Schools from './schools';
@@ -23,16 +25,22 @@ function Search() {
 
   return (
     <>
-      <Switch>
-        <Route exact path="/search/all" component={All} />
-        <Route path="/search/people" component={People} />
-        <Route path="/search/tags" component={Tags} />
-        <Route path="/search/schools" component={Schools} />
-        <Route path="/search/courses" component={Courses} />
-        <Route path="/search/notes" component={Notes} />
-        <Route path="/search/templates" component={Templates} />
-        <Route path="/" component={NoMatch} />
-      </Switch>
+      <Box pt="0">
+        <SimpleSideBar>
+          <Flex direction="column" mt={{ base: '-24', md: '-5' }}>
+            <Switch>
+              <Route exact path="/search/all" component={All} />
+              <Route path="/search/people" component={People} />
+              <Route path="/search/tags" component={Tags} />
+              <Route path="/search/schools" component={Schools} />
+              <Route path="/search/courses" component={Courses} />
+              <Route path="/search/notes" component={Notes} />
+              <Route path="/search/templates" component={Templates} />
+              <Route path="/" component={NoMatch} />
+            </Switch>
+          </Flex>
+        </SimpleSideBar>
+      </Box>
     </>
   );
 }
