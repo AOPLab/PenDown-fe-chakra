7a06b51bbf2c2caeb3c5634c9bd35473d753206b
Shannon1923 <86880073+Shannon1923@users.noreply.github.com>
2022-05-07 21:19:33 +0800

    error & loading

--- src/reducers/loading/index.js
+++ src/reducers/loading/index.js
@@ -3,10 +3,14 @@ import common from './common';
 import user from './user';
 import note from './note';
 import tag from './tag/tag';
+import school from './school/school';
+import course from './course/course';
 
 export default combineReducers({
   common,
   note,
   user,
   tag,
+  school,
+  course,
 });
