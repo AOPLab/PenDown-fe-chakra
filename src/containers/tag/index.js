37810cd6ce112768e0b8cfce363ab30b45831faf
Chienyu-1215 <b08705034@ntu.edu.tw>
2022-05-09 21:12:51 +0800

    Fix tag page

--- src/containers/tag/index.js
+++ src/containers/tag/index.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {
-  HStack, Flex, Spacer, VStack, Button, Text,
+  HStack, Flex, VStack, Text, Stack, useColorModeValue, TabList, Tab, TabPanel, Tabs, TabPanels,
 } from '@chakra-ui/react';
 
-import CardSection from '../../components/ui/CardSection';
 import StatsCard from '../../components/ui/cards/StatsCard';
 import BannerBadge from '../../components/ui/cards/BannerBadge';
+import CardSection from '../../components/ui/CardSection';
 
 function Tag() {
   const { tagId } = useParams();
-  // const history = useHistory();
+  const tags = useSelector((state) => state.tag.byId);
+
   // const location = useLocation();
   // const config = useSelector((state) => state.auth);
-  // const user = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const auth = useSelector((state) => state.auth);
+  const noteIds = useSelector((state) => state.hotNotes.hotNoteIds);
+  // 註 要改成接對應的 note
+
   const [noteType, setNoteType] = useState('Choose Note Type');
   const handleNoteTypeChange = (event) => {
     const {
@@ -23,31 +28,76 @@ function Tag() {
     setNoteType(value);
   };
 
+  const [tabIndex, setTabIndex] = useState(0);
+  const tabs = ['Popular', 'Recent'];
+  const handleTabsChange = (index) => {
+    setTabIndex(index);
+  };
+
   return (
     <>
-      <Flex direction="column" gap={10} pt={4} px={8}>
-        <Flex direction="column" align="left" gap={4} py={4} p="4">
-          <Text color="gray.600" fontWeight={600} fontSize="md">tags/</Text>
-          <Flex minWidth="60%" width="60%" alignItems="center" gap="2" flexWrap="wrap">
+      <Flex direction="column" align="left" gap={10} pt={4} px={8}>
+        <Flex direction="column" align="left" gap={4} py={4}>
+          <Text px="32px" color="gray.600" fontWeight={600} fontSize="md">
+            tag/
+          </Text>
+          <Flex alignItems="top" gap={10} flexWrap="wrap" px="32px">
             <VStack spacing={3}>
-              <BannerBadge textTransform="lowercase">#funny</BannerBadge>
-              <Button
-                variant="pendown-primary"
-                size="sm"
-                // fontSize="sm"
-              >
-                Follow
-              </Button>
+              <BannerBadge textTransform="lowercase">
+                #
+                { tags[tagId].name }
+              </BannerBadge>
             </VStack>
-            <Spacer />
             <HStack spacing={4}>
               <StatsCard title="Followers" stat="1,234" />
-              <StatsCard title="Notes" stat="9" />
+              {/* 註 stat=courses[courseId].followers */}
+              <StatsCard title="Notes" stat="1" />
+              {/* 註 stat=courses[courseId].notes */}
             </HStack>
           </Flex>
-          {/* <HStack spacing={8} mx="auto" maxW="3xl" width="80%" py={12} px={6} align="flex-start" /> */}
         </Flex>
-        <CardSection noteType={noteType} handleNoteTypeChange={handleNoteTypeChange} />
+        <Tabs isLazy size="lg" width="100%" border="hidden" variant="unstyled" index={tabIndex} onChange={handleTabsChange}>
+          <Flex justify="center" mx={['auto', 0]} mb={-2}>
+            <Stack
+              direction="row"
+              justify="space-between"
+              textAlign="center"
+              rounded="pendown"
+              bg={useColorModeValue('gray.100', 'gray.500')}
+              border="2px solid black"
+            >
+              <TabList
+                borderBottom="hidden"
+              >
+                {tabs.map((tab) => (
+                  <Tab
+                    key={tab}
+                    value={tab}
+                    _selected={{ bg: 'primary.400', borderRadius: 'pendown' }}
+                    fontSize="md"
+                    fontWeight="bold"
+                  >
+                    {tab}
+                  </Tab>
+                ))}
+              </TabList>
+            </Stack>
+          </Flex>
+          <TabPanels>
+            {tabs.map((tab) => (
+              <TabPanel key={tab}>
+                <Flex
+                  w="100%"
+                  justifyContent="center"
+                  alignItems="center"
+                  flexDirection="column"
+                >
+                  <CardSection noteType={noteType} handleNoteTypeChange={handleNoteTypeChange} noteIds={noteIds} />
+                </Flex>
+              </TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
       </Flex>
     </>
   );
