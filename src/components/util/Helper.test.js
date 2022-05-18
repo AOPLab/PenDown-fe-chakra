f9f9fe44be470b3922f00a87e0ec52426d70a4eb
Chienyu-1215 <b08705034@ntu.edu.tw>
2022-05-18 15:03:06 +0800

    Testing on Helper Functions

---
+++ src/components/util/Helper.test.js
@@ -0,0 +1,17 @@
+import { avatarSrc, statFormatting } from './Helper';
+
+test('avatarSrc Function Test', () => {
+  const name = 'TEST_NAME';
+  const expectedValue = 'https://avatars.dicebear.com/api/open-peeps/:TEST_NAME.svg?background=%23';
+  expect(avatarSrc(name)).toMatch(expectedValue);
+});
+
+test.each([
+  [1000, "1,000"],
+  [15000, "15K"],
+  [60, "60"],
+  [1832000, "1.8M"],
+  [3491, "3,491"]
+])("Num %d will has result %s", (num, expected) => {
+  expect(statFormatting(num)).toBe(expected);
+});
