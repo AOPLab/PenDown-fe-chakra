246c02ade8444f4a692bbb76b8fc5a520463c203
Brian L. Chen <brian.lxchen@gmail.com>
2022-05-20 22:15:04 +0800

    Update school page course card

--- src/components/ui/CourseSection.js
+++ src/components/ui/CourseSection.js
@@ -3,25 +3,39 @@ import { useSelector } from 'react-redux';
 import {
   SimpleGrid,
 } from '@chakra-ui/react';
+import SearchLoading from '../SearchLoading';
 
 import CourseCard from './cards/CourseCard';
 
 function CourseSection({ schoolId, courseIds }) {
   const courses = useSelector((state) => state.course.byId);
+  const loading = useSelector((state) => state.loading.common.common);
 
   return (
     <>
       <SimpleGrid
         columns={{
-          base: 1, md: 2, lg: 3,
+          base: 1, md: 1, lg: 2, xl: 2,
         }}
-        spacing={10}
+        spacing={{ base: 10, md: 12 }}
         py={4}
         pb={10}
         mx="auto"
       >
-        {courseIds.map((id) => (<CourseCard key={id} schoolId={schoolId} courseId={id} title={courses[id].name} description={courses[id].no} />))}
+        {courseIds.map((id) => (
+          <CourseCard
+            key={id}
+            schoolId={schoolId}
+            courseId={id}
+            title={courses[id].name}
+            description={courses[id].no}
+            noteCount={`${courses[id].note_cnt}`}
+          />
+        ))}
       </SimpleGrid>
+      { loading.searchCourses && (
+      <SearchLoading />
+      ) }
     </>
   );
 }
